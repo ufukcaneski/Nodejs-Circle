aadefaf23a9ef57bee7d495504de33d10ffb4561
Ufukcan Eski <ufukcan.b@hotmail.com>
2022-01-02 01:14:33 +0300

    .

---
+++ circle.js
@@ -0,0 +1,12 @@
+function circleArea(radius) {
+    console.log(Math.PI * radius * radius);
+}
+
+function circleCircumference(radius) {
+    console.log(2 * Math.PI * radius);
+}
+
+module.exports = {
+    circleArea,
+    circleCircumference
+}
